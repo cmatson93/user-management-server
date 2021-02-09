ec08b45f7c2beb674ee731dcad1b8a4c70ce27bc
Christina  Ng <cmatson93@gmail.com>
2021-02-08 22:02:43 -0800

    update with clearDb settings

--- sequelize.js
+++ sequelize.js
@@ -2,8 +2,12 @@ const Sequelize = require('sequelize');
 const UserModel = require('./models/User');
 
 const sequelize = new Sequelize('usersApp', 'root', 'password', {
-    host: 'localhost',
-    dialect: 'mysql', 
+    // host: 'localhost',
+    // dialect: 'mysql', 
+    HOST: "us-cdbr-east-03.cleardb.com",
+    USER: "b444dc7bd6d321",
+    PASSWORD: "6da63696",
+    DB: "heroku_8580e419a8c1c06"
 });
 
 const User = UserModel(sequelize, Sequelize);
